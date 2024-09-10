fc55c9c46dbbae6f4485a57056658fbc91c1b9b9
Anxheloo <anxhelocenollari@gmail.com>
2024-09-10 18:51:02 +0200

    finishing js version

---
+++ src/components/Feed.js
@@ -0,0 +1,88 @@
+import React, {useCallback, useRef, useState} from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  View,
+  Dimensions,
+  ActivityIndicator,
+  Text,
+} from 'react-native';
+import StoryComponent from './StoryComponent';
+import UseFetch from '../utils/useFetch';
+
+const windowHeight = Dimensions.get('window').height;
+
+const Feed = () => {
+  const [visibleItemIndex, setVisibleItemIndex] = useState(0);
+  const flatListRef = useRef(null);
+  const {datas, isLoading, error, refetch} = UseFetch();
+
+  const scrollTo = workoutIdx => {
+    console.log('This is workout index:', workoutIdx);
+    if (workoutIdx < datas?.length - 1) {
+      flatListRef?.current?.scrollToIndex({index: workoutIdx + 1});
+    }
+  };
+
+  const onViewableItemsChanged = useCallback(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      setVisibleItemIndex(viewableItems[0].index);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <ActivityIndicator size="large" />
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <Text>Error fetching data: {error.message}</Text>
+      </>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        ref={flatListRef}
+        refreshing={isLoading}
+        onRefresh={refetch}
+        decelerationRate="fast"
+        snapToInterval={windowHeight}
+        snapToAlignment="start"
+        showsVerticalScrollIndicator={false}
+        bounces={false}
+        overScrollMode="never"
+        data={datas}
+        keyExtractor={({id}) => id}
+        removeClippedSubviews={true}
+        initialNumToRender={5}
+        maxToRenderPerBatch={5}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={{itemVisiblePercentThreshold: 100}}
+        onEndReached={() => console.log('Ready to fetch next items ')}
+        renderItem={({item, index}) => (
+          <StoryComponent
+            stories={item}
+            scrollTo={scrollTo}
+            isVisible={index === visibleItemIndex}
+            workoutIndex={index}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Feed;
---
+++ src/helper/exportedFunction.js
@@ -0,0 +1,6 @@
+export const cross = require('../assets/white-cross.png');
+export const play = require('../assets/play.png');
+export const pause = require('../assets/pause.png');
+export const mute = require('../assets/muted.png');
+export const unmute = require('../assets/sound.png');
+export const clock = require('../assets/clock.png');
